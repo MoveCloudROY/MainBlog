86d7f36868bd10d9984c43d4e4a4fc5ddcba8ac5
movecloud <move_cloud_roy1994@qq.com>
2020-03-16 15:40:47 +0800

    Site updated: 2020-03-16 15:40:41

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,3 @@
-// build time:Mon Mar 16 2020 15:01:42 GMT+0800 (GMT+08:00)
+// build time:Mon Mar 16 2020 15:40:02 GMT+0800 (GMT+08:00)
 document.addEventListener("DOMContentLoaded",function(){if(typeof $.fn.lightGallery==="function"){$(".article").lightGallery({selector:".gallery-item"})}if(typeof $.fn.justifiedGallery==="function"){$(".justified-gallery").justifiedGallery()}});
 //rebuild by neat 
